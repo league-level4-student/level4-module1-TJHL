69df6d2d4a27dc95ec2bdd70aff07e02b2102210
League Student <league@iMac-40.attlocal.net>
2019-09-15 17:26:20 -0700

    Get remove to work

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -4,32 +4,72 @@ package _01_Custom_ArrayList;
 
 public class ArrayList <T>{
 	
+	T[] inputArray;
+	
 	public ArrayList() {
+		
+		inputArray = (T[]) new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return inputArray[loc];
 	}
 	
 	public void add(T val) {
+		
+		T[] tempAdd = (T[])new Object[inputArray.length+1];
+		tempAdd[tempAdd.length-1]=val;
+		for (int i = 0; i < inputArray.length; i++) {
+			tempAdd[i]=inputArray[i];
+		}
+		inputArray = tempAdd;
 		
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
 		
+		T[] tempInsert = (T[])new Object[inputArray.length+1];
+		for (int i = 0; i < loc; i++) {
+			tempInsert[i]=inputArray[i];
+		}
+		
+			tempInsert[loc]= val;
+			
+		for (int i = loc+1; i < tempInsert.length; i++) {
+			tempInsert[i]=inputArray[i-1];
+		}
+		
+		inputArray =tempInsert;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
+
+		inputArray[loc]=val;
 		
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
+		
+		T[] tempRemove = (T[])new Object [inputArray.length-1];
+		
+		for (int i = 0; i < loc; i++) {
+			tempRemove[i]=inputArray[i];
+		}
+		
+		for (int i = loc; i < tempRemove.length; i++) {
+			tempRemove[i] = inputArray[i];
+		}
+		inputArray = tempRemove;
 		
 	}
 	
 	public boolean contains(T val) {
 		
 		return false;
+	}
+	
+	public int size() {
+		return 0;
 	}
 }
